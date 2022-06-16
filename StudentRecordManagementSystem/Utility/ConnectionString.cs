c4c283815b7144ab66a721d8e6292925334fbda7
Mohd Mustkeem <mohdmustkeem78624@gmail.com>
2022-06-16 16:47:41 +0530

    Add project files.

---
+++ StudentRecordManagementSystem/Utility/ConnectionString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentRecordManagementSystem.Utility
+{
+    public static class ConnectionString
+    {
+        //private static string cName = "Data Source=DESKTOP-L91OLLD; Initial Catalog=StudentManagement;";
+        private static string cName = "Server=DESKTOP-L91OLLD;Database=StudentManagement;Trusted_Connection=True;MultipleActiveResultSets=true";
+        public static string CName
+        {
+            get => cName;
+        }
+    }
+}
